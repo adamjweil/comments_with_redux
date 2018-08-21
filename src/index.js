b01333445a2808887e79daf28d6bd835a7b35663
Adam Weil <adamjweil@gmail.com>
2018-08-20 20:27:30 -0400

    adding all the required dependencies

--- src/index.js
+++ src/index.js
@@ -4,5 +4,10 @@ import './index.css';
 import App from './components/App';
 import registerServiceWorker from './registerServiceWorker';
 
+import { render } from 'react-dom';
+import { createStore } from 'redux';
+import { Provider } from 'react-redux';
+import rootReducer from './reducers';
+
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
